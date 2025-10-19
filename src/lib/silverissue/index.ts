64b0e31d0d8ba137136a8ca8477c90c76df24ee4
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-10-19 16:49:57 +0200

    add silverissue

---
+++ src/lib/silverissue/index.ts
@@ -0,0 +1,24 @@
+import { ErrorRequestHandler } from "express";
+import { Webhook } from "./src/webhook";
+
+const webhook = new Webhook('https://discord.com/api/webhooks/1429472000253890601/vw_Kx1KoHjig2I05iumB9WhyT0nSvh78n2eEclUxd19RNsieJ8khUowjcfmCKtWSxTUD');
+
+process.on("uncaughtException", async (error) => {
+    console.error("Erreur non gérée :", error);
+    await webhook.sendError(error, "uncaughtException");
+});
+
+process.on("unhandledRejection", async (reason) => {
+    console.error("Rejet non géré :", reason);
+    await webhook.sendError(reason, "unhandledRejection");
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next): void => {
+    webhook.sendError(err, `Route : ${req.method} ${req.url}`);
+    next();
+}
+
+export {
+    webhook,
+    errorHandler as SilverIssueMiddleware
+}
