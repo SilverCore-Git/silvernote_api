920261ee2e348334c0c1436158f0e27711db7063
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-10-22 21:11:53 +0200

    add sk for mobile fetch

--- src/middleware/AllowedOriginCheck.ts
+++ src/middleware/AllowedOriginCheck.ts
@@ -1,5 +1,6 @@
 import  type { NextFunction, Response, Request } from "express";
 import config from '../config.json';
+import { error } from "console";
 
 export default function  AllowedOriginCheck (req: Request, res: Response, next: NextFunction) 
 {
@@ -7,6 +8,31 @@ export default function  AllowedOriginCheck (req: Request, res: Response, next: 
     const allowedOrigins = config.corsOptions.origin;
 
     const origin = req.headers.origin;
+    const client_sk = req.headers.authorization;
+    const cloud_sk = process.env.MOBILE_SECRET_KEY;
+
+    if (client_sk)
+    {
+        if (!cloud_sk) 
+        {
+            res.status(502).json({ error: true, message: 'cloud sk not found' });
+            return;
+        }
+
+        if (client_sk !== client_sk) 
+        {
+            res.status(402).json({ error: true, message: 'client and cloud sk doesn\'t mach' });
+            return;
+        }
+
+        if (cloud_sk && client_sk === cloud_sk) 
+        {
+            return next();
+        }
+
+        res.status(501).json({ error: true, message: 'An error has alive' });
+        return;
+    }
 
     if (!origin || origin && !allowedOrigins.includes(origin)) {
         res.status(403).json({ error: true, message: "Invalid or missing origin" });
