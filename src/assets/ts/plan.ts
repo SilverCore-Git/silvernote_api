667525793aeac523b9e485c131b34f4f1a3fd42f
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-10-24 11:09:25 +0200

    add benefits type and benefits const and get_benefits_by_planId in plan.ts

--- src/assets/ts/plan.ts
+++ src/assets/ts/plan.ts
@@ -13,8 +13,54 @@ export interface Plan {
         family_data?: {
             owner?: boolean;
         }
-    }
+    };
+    benefits?: Benefits;
 };
+
+export interface Benefits {
+    planId: UUID;
+    notesLength: number;
+    tagsLength: number;
+    SilverAI: boolean;
+    SilverAILengthPerDay: number;
+    ShareNote: boolean;
+}
+
+
+const benefits: Benefits[] = [
+    {
+        planId: "e9009da1-e1e6-448d-b264-353ba8c0a850", // silver
+        notesLength: 100,
+        tagsLength: 20,
+        SilverAI: false,
+        SilverAILengthPerDay: 0,
+        ShareNote: false
+    },
+    {
+        planId: "7350f460-ed5c-4286-b598-85b894d2616a", // gold
+        notesLength: 200,
+        tagsLength: 50,
+        SilverAI: true,
+        SilverAILengthPerDay: 50,
+        ShareNote: true
+    },
+    {
+        planId: "4c1c20f8-06b7-4e29-afb4-38d4b482e302", // platinium
+        notesLength: 1000,
+        tagsLength: 100,
+        SilverAI: true,
+        SilverAILengthPerDay: 100,
+        ShareNote: true
+    },
+    {
+        planId: "e5643c29-cb91-4c9a-a14b-ea5d9919a47d", // ultimate
+        notesLength: 999999999,
+        tagsLength: 999999999,
+        SilverAI: true,
+        SilverAILengthPerDay: 999999999,
+        ShareNote: true
+    }
+];
 
 
 const plans: Plan[] = [
@@ -49,6 +95,11 @@ const plans: Plan[] = [
 export const get_silver_plan = (): Plan => {
     return plans.filter(plan => plan.name == 'silver')[0];
 } 
+
+export function get_benefits_by_planId (planId: UUID): Benefits | undefined
+{
+    return benefits.find(benef => benef.planId === planId);
+}
 
 
 export function get_plan_by_name(
