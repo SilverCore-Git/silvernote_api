0d1574d98570ecff299213377601df1d3543470e
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-01-18 10:22:38 +0100

    Update useWS.ts

--- src/mcp-server/src/utils/useWS.ts
+++ src/mcp-server/src/utils/useWS.ts
@@ -1,11 +1,15 @@
 import { io, Socket } from "socket.io-client";
 
-const useWSocket = () => {
+const useWSocket = (room: string) => {
 
     const socket: Socket = io('http://localhost:3434', {
         path: "/socket.io/share",
         transports: ["websocket", "polling"],
         autoConnect: true
+    });
+
+    socket.on('connect', () => {
+        socket.emit('join-room', room);
     });
 
     return {
