1076201bd6a942d509b476f9091ec15d37461c25
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-01-18 11:28:23 +0100

    Create add_note_tag.ts

---
+++ src/mcp-server/src/tools/add_note_tag.ts
@@ -0,0 +1,48 @@
+import notes from "../../../assets/ts/notes.js";
+import { Tool } from "../../MCPTypes.js";
+import useWS from "../utils/useWS.js";
+import { z } from "zod";
+
+
+const add_note_tag: Tool = {
+
+    name: "add_note_tag",
+    description: "Add a tag on a note with tag id",
+
+    params: {
+        uuid: z.string().describe('note uuid'),
+        tagId: z.string().describe('tag id'),
+    },
+
+    handler: async (parms) => {
+
+        const note = (await notes.getNoteByUUID(parms.uuid)).note;
+
+        if (!note) return {
+            content: [
+                {
+                    type: "text",
+                    text: 'Note introuvable.',
+                },
+            ],
+        };
+
+        note.tags?.push(parms.tagId);
+
+        await notes.updateNote(note);
+        
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: 'Tag ajouté a la note.',
+                },
+            ],
+        };
+
+    }
+
+}
+
+
+export default add_note_tag;
