21d3e8e2ca8cc09710bfa8b5d4b2ed9e8cadcb44
mister_papaye <philippeetchtebaise@hotmail.com>
2026-02-04 17:50:32 +0100

    Update news.ts

--- src/assets/ts/news.ts
+++ src/assets/ts/news.ts
@@ -1,20 +1,37 @@
 import fs from 'fs';
 import path from 'path';
 import __dirname from './_dirname.js';
+import { UUID } from 'crypto';
 
 interface NewsItem {
-    id: number;
+    id: UUID;
     title: string;
     content: string; // md format
+    readBy: string[];
     date: Date;
 }
+
+const newsFilePath = path.join(__dirname, '../../', 'db', 'news.json');
+
 
 class News
 {
 
+    constructor()
+    {
+        if (!fs.existsSync(newsFilePath)) {
+            fs.writeFileSync(newsFilePath, JSON.stringify([], null, 2), 'utf-8');
+        }
+    }
+
     private getNewsFile (): NewsItem[]
     {
-        return JSON.parse(fs.readFileSync(path.join(__dirname, '../config/news.json'), 'utf-8'));
+        return JSON.parse(fs.readFileSync(newsFilePath, 'utf-8'));
+    }
+
+    private async saveNewsFile (news: NewsItem[]): Promise<void>
+    {
+        await fs.promises.writeFile(newsFilePath, JSON.stringify(news, null, 2), 'utf-8');
     }
 
     public getAllNews (): NewsItem[]
@@ -26,6 +43,21 @@ class News
     {
         const news = this.getNewsFile();
         return news.slice(0, count);
+    }
+
+    public async markNewsAsRead (newsId: string, userId: string): Promise<void>
+    {
+
+        const news = this.getNewsFile();
+        const index = news.findIndex(n => n.id === newsId);
+
+        if (index !== -1) {
+            if (news[index].readBy.includes(userId)) return;
+            news[index].readBy.push(userId);
+            console.log(`User ${userId} marked news ${newsId} as read, ${news[index].readBy.length} total reads.`);
+            await this.saveNewsFile(news);
+        }
+
     }
 
 }
