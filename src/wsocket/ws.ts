e204d50a74114b46c6d0786cdc84878e7924918c
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-03-04 10:49:07 +0100

    fix ws

--- src/wsocket/ws.ts
+++ src/wsocket/ws.ts
@@ -3,7 +3,7 @@ import { createServer } from 'http';
 import fs from 'fs';
 import path from 'path';
 import __dirname from "../assets/ts/_dirname.js";
-const config = JSON.parse(fs.readFileSync(path.join(__dirname, '../../../config.json'), 'utf-8'))
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, '../../config.json'), 'utf-8'))
 import routes from "./routes/index.js";
 import { verifyToken } from "@clerk/express";
 
